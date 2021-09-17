6ce972650a5e117409d5d547d94564a7dbb3b7bb
Gali Saketh <sakethgali@gmail.com>
2021-09-17 10:35:48 -0700

    CustomerSignUp Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';
+import CustomerSignUp from './Components/CustomerSignUp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Welcome to React
-        </p>
+      <header>
+        <CustomerSignUp/>
       </header>
     </div>
   );
